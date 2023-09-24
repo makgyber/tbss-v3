92e03be08f50f7bb615392c610b796d89d5ac745
Jon Mereria <makgyber@gmail.com>
2023-09-24 15:12:57 +0800

    apex charts

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -4,9 +4,12 @@ namespace App\Filament\Pages;
 
 use App\Filament\Widgets\CallLogs;
 use App\Filament\Widgets\DailyInboundInquiries;
+use App\Filament\Widgets\ExtensionLeadsFunnel;
 use App\Filament\Widgets\LeadsByServiceTypeChart;
 use App\Filament\Widgets\LeadsBySourceChart;
 use App\Filament\Widgets\LeadsChart;
+use App\Filament\Widgets\ProspectiveLeadsFunnel;
+use App\Filament\Widgets\RenewalLeadsFunnel;
 use Filament\Pages\Dashboard as BasePage;
 
 class Dashboard extends BasePage
@@ -30,6 +33,9 @@ class Dashboard extends BasePage
         return [
             LeadsBySourceChart::class,
             LeadsByServiceTypeChart::class,
+            ProspectiveLeadsFunnel::class,
+            RenewalLeadsFunnel::class,
+            ExtensionLeadsFunnel::class,
             // LeadsChart::class
             // ExpiringContracts::class,
             // PendingContracts::class,
